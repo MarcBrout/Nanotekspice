c3ee0b9eb5e58114b8092d9c0a60b459af3cec68
MarcBrout <marc.brout@epitech.eu>
2017-02-03 19:19:53 +0100

    ComponentFactory done

--- src/component/ComponentFactory.cpp
+++ src/component/ComponentFactory.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "ComponentFactory.hh"
+#include "Component4001.hh"
+
+nts::IComponent *nts::ComponentFactory::createComponent(std::string const &type, std::string const &value)
+{
+  std::map<std::string, nts::ComponentFactory::createCompPtr>::const_iterator cit;
+
+  if (cmpBuilder.empty())
+    cmpBuilder = createMap();
+
+  cit = cmpBuilder.find(type);
+  if (cit == cmpBuilder.end())
+    return (NULL);
+  return (this->*cmpBuilder[type](value));
+}
+
+std::map<std::string, nts::ComponentFactory::createCompPtr> nts::ComponentFactory::createMap() const
+{
+  std::map<std::string, nts::ComponentFactory::createCompPtr> map {
+      {"4001", &create4001}
+  };
+
+}
+
+nts::IComponent *nts::ComponentFactory::create4001(std::string const &value) const
+{
+  return (new Component4001(value));
+}
