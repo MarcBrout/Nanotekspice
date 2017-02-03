94214da73dc1119f94e37b8a1d5be1f99cc949b8
Benjamin Duhieu <benjamin.duhieu@epitech.eu>
2017-02-03 18:52:05 +0100

    Begin of parser, lexer ok

---
+++ src/Parser/Parser.hh
@@ -0,0 +1,66 @@
+//
+// Created by duhieu_b on 2/2/17.
+//
+
+#ifndef CPP_NANOTEKSPICE_PARSER_HH
+#define CPP_NANOTEKSPICE_PARSER_HH
+
+
+#include <iostream>
+#include <vector>
+#include "IParser.hh"
+
+namespace nts
+{
+    typedef void (nts::Parser::*FuncPtr)(void);
+
+    class Parser : public nts::IParser
+    {
+    private:
+        nts::t_ast_node *tree;
+        std::vector<std::string> lex;
+        std::string nameComponent;
+        std::string nameType;
+        std::string linkName;
+        std::string linkValue;
+        std::string commentString;
+        static std::map<std::string, nts::FuncPtr> myLexMap;
+
+        std::map<std::string, nts::FuncPtr> create_map();
+    protected:
+        void createNode(std::string &it);
+
+        void section(std::string sect);
+        void chipsets();
+        void links();
+
+        void newLine();
+
+        //components
+
+        void input();
+        void output();
+        void clock();
+        void trues();
+        void falses();
+        void componentName();
+
+        void link();
+
+        void link_end();
+
+        void comment();
+    public:
+        Parser();
+
+        virtual ~Parser();
+
+        void feed(std::string const &input) override;
+
+        void parseTree(nts::t_ast_node &root) override;
+
+        nts::t_ast_node *createTree() override;
+    };
+}
+
+#endif //CPP_NANOTEKSPICE_PARSER_HH
