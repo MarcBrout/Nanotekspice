04246ff815657041e0b95df1afbc136935b1b479
marc brout <marc.brout@epitech.eu>
2017-02-08 16:08:09 +0100

    pushed unit-test of input and output + linked output to one output unit tests

---
+++ units/tests-component4011.cpp
@@ -0,0 +1,53 @@
+//
+// Created by brout_m on 08/02/17.
+//
+
+#include <cassert>
+#include <iostream>
+#include "ComponentFactory.hh"
+#include "Component4011.hh"
+
+int main() {
+    nts::ComponentFactory   factory;
+    nts::IComponent         *iCmp4011 = factory.createComponent("4011", "TestingComponent4011");
+    nts::AComponent         &aCmp4011 = static_cast<nts::AComponent &>(*iCmp4011);
+
+    std::cout << "Testing initial state of all pins";
+
+    for (size_t i = 1; i <= 13; ++i) {
+        assert(aCmp4011[i] == nts::UNDEFINED);
+        std::cout << ".";
+    }
+
+    assert(aCmp4011.Compute(3) == nts::UNDEFINED);
+    assert(aCmp4011.Compute(4) == nts::UNDEFINED);
+    assert(aCmp4011.Compute(10) == nts::UNDEFINED);
+    assert(aCmp4011.Compute(11) == nts::UNDEFINED);
+    std::cout << ".";
+
+    std::cout << "\nAll pins correctly initialized!\n";
+    std::cout << "Testing all nand gates";
+
+    aCmp4011[1] = nts::TRUE;
+    aCmp4011[2] = nts::TRUE;
+    assert(aCmp4011.Compute(3) == nts::FALSE);
+    std::cout << ".";
+
+    aCmp4011[5] = nts::FALSE;
+    aCmp4011[6] = nts::TRUE;
+    assert(aCmp4011.Compute(4) == nts::TRUE);
+    std::cout << ".";
+
+    aCmp4011[8] = nts::TRUE;
+    aCmp4011[9] = nts::FALSE;
+    assert(aCmp4011.Compute(10) == nts::TRUE);
+    std::cout << ".";
+
+    aCmp4011[12] = nts::FALSE;
+    aCmp4011[13] = nts::FALSE;
+    assert(aCmp4011.Compute(11) == nts::TRUE);
+    std::cout << ".";
+    std::cout << "\nSuccess!\n";
+
+    return (0);
+}
