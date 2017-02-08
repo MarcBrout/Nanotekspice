522326164e847a9ed9a036a61a91c41a13c6b7f8
marc brout <marc.brout@epitech.eu>
2017-02-08 13:14:44 +0100

    unit tests for 4001, 4008

--- units/tests-component4008.cpp
+++ units/tests-component4008.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <cassert>
+#include <iostream>
 #include "ComponentFactory.hh"
 #include "Component4008.hh"
 
@@ -11,9 +12,13 @@ int main() {
     nts::IComponent         *iCmp4008 = factory.createComponent("4008", "TestingComponent4008");
     nts::AComponent         &aCmp4008 = static_cast<nts::AComponent &>(*iCmp4008);
 
+    std::cout << "Testing initial state of all Pins";
 
-    for (size_t i = 1; i <= 15; ++i)
+    for (size_t i = 1; i <= 15; ++i) {
+        std::cout << ".";
         assert(aCmp4008[i] == nts::UNDEFINED);
+    }
+
 
     assert(aCmp4008.Compute(10) == nts::UNDEFINED);
     assert(aCmp4008.Compute(11) == nts::UNDEFINED);
@@ -21,46 +26,87 @@ int main() {
     assert(aCmp4008.Compute(13) == nts::UNDEFINED);
     assert(aCmp4008.Compute(14) == nts::UNDEFINED);
 
+    std::cout << "\nAll pins correctly initialized!\n";
+    std::cout << "Testing internal adding gates";
+
     aCmp4008[6] = nts::TRUE;
     aCmp4008[7] = nts::TRUE;
     aCmp4008[9] = nts::TRUE;
     assert(aCmp4008.Compute(10) == nts::TRUE);
     assert(aCmp4008.Compute(14) == nts::TRUE);
+    std::cout << ".";
 
     aCmp4008[4] = nts::FALSE;
     aCmp4008[5] = nts::TRUE;
     assert(aCmp4008.Compute(11) == nts::FALSE);
     assert(aCmp4008.Compute(14) == nts::TRUE);
+    std::cout << ".";
 
     aCmp4008[2] = nts::TRUE;
     aCmp4008[3] = nts::FALSE;
     assert(aCmp4008.Compute(12) == nts::FALSE);
     assert(aCmp4008.Compute(14) == nts::TRUE);
+    std::cout << ".";
 
     aCmp4008[15] = nts::FALSE;
     aCmp4008[1] = nts::FALSE;
     assert(aCmp4008.Compute(13) == nts::TRUE);
     assert(aCmp4008.Compute(14) == nts::FALSE);
+    std::cout << ".";
 
     aCmp4008[6] = nts::FALSE;
     aCmp4008[7] = nts::FALSE;
     aCmp4008[9] = nts::FALSE;
     assert(aCmp4008.Compute(10) == nts::FALSE);
     assert(aCmp4008.Compute(14) == nts::FALSE);
+    std::cout << ".";
 
     aCmp4008[4] = nts::FALSE;
     aCmp4008[5] = nts::TRUE;
     assert(aCmp4008.Compute(11) == nts::TRUE);
     assert(aCmp4008.Compute(14) == nts::FALSE);
+    std::cout << ".";
 
     aCmp4008[2] = nts::TRUE;
     aCmp4008[3] = nts::FALSE;
     assert(aCmp4008.Compute(12) == nts::TRUE);
     assert(aCmp4008.Compute(14) == nts::FALSE);
+    std::cout << ".";
 
     aCmp4008[15] = nts::TRUE;
     aCmp4008[1] = nts::TRUE;
     assert(aCmp4008.Compute(13) == nts::FALSE);
     assert(aCmp4008.Compute(14) == nts::TRUE);
+    std::cout << ".";
+
+    std::cout << "\nAll gates working!";
+    std::cout << "\nTesting Computing of last Pin";
+
+    for (size_t i = 1; i <= 15; ++i) {
+        std::cout << ".";
+        assert(aCmp4008[i] = nts::UNDEFINED);
+    }
+
+    aCmp4008[6] = nts::TRUE;
+    aCmp4008[7] = nts::FALSE;
+    aCmp4008[9] = nts::TRUE;
+    aCmp4008[4] = nts::FALSE;
+    aCmp4008[5] = nts::FALSE;
+    aCmp4008[2] = nts::FALSE;
+    aCmp4008[3] = nts::TRUE;
+    aCmp4008[15] = nts::TRUE;
+    aCmp4008[1] = nts::TRUE;
+    std::cout << ".";
+
+    assert(aCmp4008.Compute(13) == nts::FALSE);
+    std::cout << ".";
+    assert(aCmp4008.Compute(14) == nts::TRUE);
+    std::cout << ".";
+    assert(aCmp4008[14] == nts::TRUE);
+    assert(aCmp4008[12] == nts::TRUE);
+    assert(aCmp4008[11] == nts::TRUE);
+    assert(aCmp4008[10] == nts::FALSE);
+    std::cout << ".";
+    std::cout << "\nSuccess!\n";
     return (0);
 }
