e7723000d65cd7c7c4c2474f0ba8333cc9e07c71
marc brout <marc.brout@epitech.eu>
2017-03-04 21:31:53 +0100

    4013 correction

--- src/component/Component4013.cpp
+++ src/component/Component4013.cpp
@@ -11,19 +11,20 @@ nts::Component4013::Component4013(std::string const &name) :
         AComponent(name,
                    nts::COMPONENT4013,
                    14,
-                   {3, 4, 5, 6, 8, 9, 10, 11},
-                   {1, 2, 12, 13},
-                   {{1, {3, 4, 5, 6}},
-                    {2, {3, 4, 5, 6}},
-                    {12, {8, 9, 10, 11}},
-                    {13, {8, 9, 10, 11}}
+                   {CP1, CP2, RESET1, RESET2, DATA1, DATA2, SET1, SET2},
+                   {KQ1, NQ1, KQ2, NQ2},
+                   {{KQ1, {CP1, RESET1, DATA1, SET1}},
+                    {NQ1, {CP1, RESET1, DATA1, SET1}},
+                    {KQ2, {CP2, RESET2, DATA2, SET2}},
+                    {NQ2, {CP2, RESET2, DATA2, SET2}}
                    })
 {
 }
 
 nts::Tristate nts::Component4013::Compute(size_t pin_num_this) {
     nts::Tristate                       state;
-    std::vector<nts::Output>::iterator  it;
+    nts::Tristate                       clockState1;
+    nts::Tristate                       clockState2;
 
     if (!isPinOk(pin_num_this))
         throw std::logic_error(Name + " doesn't have a '" + std::to_string(pin_num_this) + "' pin");
@@ -31,13 +32,27 @@ nts::Tristate nts::Component4013::Compute(size_t pin_num_this) {
     if (isInPinList(InPins, pin_num_this)) {
         state = getPinLinkedInput(pin_num_this);
     } else {
+        clockState1 = Pins[CP1];
+        clockState2 = Pins[CP2];
         ComputeRequiredPins(pin_num_this);
-        switch (pin_num_this) {
-            case 1 : state = Pins[1] = Gates::flipflop(Pins[3], Pins[4], Pins[5], Pins[6], Pins[1], Pins[2], true); break;
-            case 2 : state = Pins[2] = Gates::flipflop(Pins[3], Pins[4], Pins[5], Pins[6], Pins[1], Pins[2], false); break;
-            case 12 : state = Pins[12] = Gates::flipflop(Pins[11], Pins[10], Pins[9], Pins[8], Pins[13], Pins[12], false); break;
-            case 13 : state = Pins[13] = Gates::flipflop(Pins[11], Pins[10], Pins[9], Pins[8], Pins[13], Pins[12], true); break;
-            default: state = UNDEFINED;
+        if (pin_num_this == KQ1 || pin_num_this == NQ1) {
+            if (Pins[CP1] == TRUE && (clockState1 == FALSE || clockState1 == UNDEFINED)) {
+                if (pin_num_this == KQ1)
+                    state = Pins[1] = Gates::flipflop(Pins[3], Pins[4], Pins[5], Pins[6], Pins[1], Pins[2], true);
+                else
+                    state = Pins[2] = Gates::flipflop(Pins[3], Pins[4], Pins[5], Pins[6], Pins[1], Pins[2], false);
+            } else {
+                state = Pins[pin_num_this];
+            }
+        } else {
+            if (Pins[CP2] == TRUE && (clockState2 == FALSE || clockState2 == UNDEFINED)) {
+                if (pin_num_this == KQ2)
+                    state = Pins[12] = Gates::flipflop(Pins[11], Pins[10], Pins[9], Pins[8], Pins[13], Pins[12], false);
+                else
+                    state = Pins[13] = Gates::flipflop(Pins[11], Pins[10], Pins[9], Pins[8], Pins[13], Pins[12], true);
+            } else {
+                state = Pins[pin_num_this];
+            }
         }
     }
     return (state);
